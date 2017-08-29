177e9f0447df4b07aba1e1caad0d36bb65ff3099
Ryan Deeds <ryancdeeds@gmail.com>
2017-08-29 12:24:20 -0500

    working on polishing the texts.

--- AKtwilio.py
+++ AKtwilio.py
@@ -5,7 +5,7 @@ from flask import Flask, request, redirect, Response, redirect
 from twilio.rest import Client
 
 app = Flask(__name__)
-client = Client(ACCOUNT_SID, AUTH_TOKEN)  # twilio connection
+client = Client(ACCOUNT_SID, AUTH_TOKEN)  # twilio connection www.twilio.com - get a number
 
 phasedb = TinyDB('phase.json')  # www.tinydb.com holds the phases per user
 hashdb = TinyDB('hash.json')  # holds the returned results so hash can be used
@@ -45,16 +45,19 @@ def twilio_send(phonenumber,message):
 
 def entrypoint(phonenumber, message):
     '''Twilio sends input here we grab number and see what phase use is in'''
+    print('Inside Entry point')
     phase = None
     output=''
     for item in phasedb:
         if item['phonenumber'] == phonenumber:
             phase = item['phase']
-            if phase == None:
-                phasedb.insert({'phonenumber': phonenumber, 'phase': 0})
-                phase = 0
-        output=router(phonenumber, message, phase)
-        return output
+            print(phase)
+    if phase == None:
+        phasedb.insert({'phonenumber': phonenumber, 'phase': 0})
+        phase = 0
+
+    output=router(phonenumber, message, phase)
+    return output
 
 
 def kodiakwizard():
@@ -72,7 +75,7 @@ def whatbusiness(phonenumber):
 
 def specificbusiness(phonenumber, term):
     '''10 search results returned for users in phase 1 '''
-    twilio_send(phonenumber,'Please wait while we grab the list')
+    twilio_send(phonenumber,'Please wait while we grab the list - it takes a few seconds - check out www.askkodiak.com while you wait')
     output = '\n\nWhich specific industry does the client fall most closely into from the above list?(Enter the number or 0 to search again)\n\n'
     r = requests.get('https://api.askkodiak.com/v1/search/:' + term, headers=headers, auth=(GROUP_ID, KEY))
     returnfromak = r.json()
@@ -82,7 +85,7 @@ def specificbusiness(phonenumber, term):
     for hitcount, item in enumerate(hits):
         output = output + str((hitcount + 1)) + ' ' + item['description'] + '\n'
 
-        hashdb.insert({'phonenumber': phonenumber, 'id': hitcount + 1, 'hash': item['hash']})
+        hashdb.insert({'phonenumber': phonenumber, 'id': hitcount + 1, 'hash': item['hash'],'desc': item['description']})
 
     query = Query()
     phasedb.update({'phase': 2}, query.phonenumber == phonenumber)
@@ -92,10 +95,12 @@ def specificbusiness(phonenumber, term):
 def finalresults(phonenumber, numericresult):
     '''send user list of carriers - phase 2 and reset phase'''
     hash = ''
+    desc =''
     for item in hashdb:
         if item['id'] == int(numericresult):
             hash = item['hash']
-    twilio_send(phonenumber, 'Please wait while we grab products - visit www.askkodiak.com for more!')
+            desc=item['desc']
+    twilio_send(phonenumber, 'Retrieving products for {} - visit www.askkodiak.com for more!'.format(desc))
     r = requests.get(
         'https://api.askkodiak.com/v1/products/class-code/naics/' + hash, headers=headers, auth=(GROUP_ID, KEY))
 
@@ -116,7 +121,7 @@ def finalresults(phonenumber, numericresult):
         carrierphone = owner.get('phone', 'No Phone')
         carriersite = owner.get('website', 'No site')
 
-        output = output + lob + ' by ' + carriername + '. ' + ambestrating + ' ' + carrierphone + ' ' + carriersite + '\n\n'
+        output = output + lob + ' by ' + carriername + '. ' + ambestrating + ' ' + carrierphone + ' ' + carriersite + '  \n\n'
 
     print(output)
     query = Query()
